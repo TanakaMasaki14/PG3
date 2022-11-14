9153b8b49fcc46abeb24d0580e77bae5d5fce464
tanaka_masaki <k021g1449@g.neec.ac.jp>
2022-11-14 10:55:34 +0900

    1-4

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <functional>
+
+void SetTimeout(std::function<void()> func, int second)
+{
+	Sleep(second);
+	func();
+}
+
+int main(int argc, const char* argv[])
+{
+	//“ü—Í
+	int select;
+	printf("“ü—Í:‹ô”‚È‚ç0 Šï”‚È‚ç1\n");
+	scanf_s("%d", &select);
+
+	//“š‚¦‚ÌŒˆ’è
+	int ans = 0;
+	srand(time(nullptr));
+
+	ans = rand() % 100;
+
+	//ŠÖ”‚Ìì¬
+	std::function<void()> judgeFunc = [&]()
+	{
+
+		printf("%d\n", ans);
+		if (ans % 2 == 0 and select == 0)
+		{
+			printf("³‰ğ\n");
+		}
+		else if (ans % 2 == 1 and select == 1)
+		{
+			printf("³‰ğ\n");
+		}
+		else
+		{
+			printf("•s³‰ğ\n");
+		}
+
+	};
+
+	//Às
+	SetTimeout(judgeFunc, 2700);
+
+	return 0;
+}
