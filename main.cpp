8e3ffff150321e50eed38d5e57f3e2992854713b
tanaka_masaki <k021g1449@g.neec.ac.jp>
2022-12-05 11:33:08 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -1,89 +1,87 @@
 #include <stdio.h>
-#include <iostream>
 #include <string>
-
-using namespace std;
+#include <iostream>
 
 typedef struct cell
 {
 	char val[64];
-	struct cell* prev;
 	struct cell* next;
+	struct cell* preview;
 }CELL;
 
-CELL* getInsertCellAddress(CELL* endCELL, int iterator);
+using namespace std;
+
+CELL* CellAddress(CELL* endCELL, int iterator);
 void create(CELL* begin, char val[64]);
-void erase(CELL* currentCell);
-void replace(CELL* currentCell, char val[64]);
-void swap(CELL* CellA, CELL* CellB);
+void out(CELL* justCell);
+void change(CELL* cellA, CELL* cellB);
+void move(CELL* justCell, char val[64]);
 void index(CELL* begin);
 int GetSize(CELL* endCell);
-void StatingMenu();
+void Begining();
 
 int main()
 {
-	int inputValue;
-
-	int selectedControl = 0;
-
-	CELL head;
-	head.prev = nullptr;
-	head.next = nullptr;
+	int InputVal;
+	int select = 0;
+	CELL initial;
+	initial.next = nullptr;
+	initial.preview = nullptr;
 
 	while (true)
 	{
-		switch (selectedControl)
+		switch (select)
 		{
 		default:
 			printf("\n[要素の操作]\n1. 要素の表示\n2. 要素の挿入\n");
-			if (GetSize(&head) > 0)
+			if (GetSize(&initial) > 0)
 			{
 				printf("3. 要素の編集\n4. 要素の削除\n5. 要素の並び替え(オプション)\n");
 			}
-			printf("\n-------------------------------\n操作を選択してください\n");
-			scanf_s("%d", &inputValue);
-			selectedControl = inputValue;
+			printf("\n------------------------\n操作を選択してください\n");
+			scanf_s("%d", &InputVal);
+			select = InputVal;
 			break;
 
 		case 1:
 			printf("\n[要素の表示]\n1. 要素の一覧表示\n2. 順番を指定して要素を表示\n9. 要素選択に戻る\n\n操作を選択してください\n");
-			scanf_s("%d", &inputValue);
-			switch (inputValue)
+			scanf_s("%d", &InputVal);
+			switch (InputVal)
 			{
 			case 1:
 				printf("\n[要素の一覧表示]\n要素一覧: {\n");
-				index(&head);
-				printf("}\n\n要素数: %d\n", GetSize(&head));
-				StatingMenu();
-				scanf_s("%d", &inputValue);
-				if (inputValue == 2)
+				index(&initial);
+				printf("}\n\n要素数: %d\n", GetSize(&initial));
+				Begining();
+				scanf_s("%d", &InputVal);
+				if (InputVal == 2)
 				{
-					selectedControl = 0;
+					select = 0;
 				}
 				break;
 
 			case 2:
 				printf("\n[順番を指定して要素を表示]\n表示したい要素の順番を指定ください。\n");
-				scanf_s("%d", &inputValue);
+				scanf_s("%d", &InputVal);
 
-				if (GetSize(&head) > inputValue && inputValue >= 0)
+				if (GetSize(&initial) > InputVal && InputVal >= 0)
 				{
-					printf("\n%d: %s\n\n", inputValue, getInsertCellAddress(&head, inputValue + 1)->val);
+					printf("\n%d: %s\n\n", InputVal, CellAddress(&initial, InputVal + 1)->val);
 				}
 				else
 				{
-					printf("\n%d番目の要素が見つかりませんでした。\n", inputValue);
+					printf("\n%d番目の要素が見つかりませんでした。\n", InputVal);
 				}
-				StatingMenu();
-				scanf_s("%d", &inputValue);
-				if (inputValue == 2)
+				Begining();
+				scanf_s("%d", &InputVal);
+				if (InputVal == 2)
 				{
-					selectedControl = 0;
+					select = 0;
 				}
 				break;
 
 			case 9:
-				selectedControl = 0;
+				select = 0;
 			}
 			break;
 
@@ -92,48 +90,47 @@ int main()
 			int itr;
 			char val[64];
 
-			string getSelect;
+			string GetSelect;
 			printf("\n[要素の挿入]\n要素を追加する場所を指定してください。最後尾に追加する場合は何も入力しないでください。\n");
 
 			cin.clear();
 			cin.ignore(1024, '\n');
-			getline(cin, getSelect);
+			getline(cin, GetSelect);
 
-			if (getSelect == "")
+			if (GetSelect == "")
 			{
-				itr = GetSize(&head);
+				itr = GetSize(&initial);
 			}
 			else
 			{
-				itr = stoi(getSelect);
+				itr = stoi(GetSelect);
 			}
 
-			itr = max(min(itr, GetSize(&head)), 0);
+			itr = max(min(itr, GetSize(&initial)), 0);
 
 			printf("\n追加する要素の値を入力してください。\n");
 			scanf_s("%s", val, 64);
 
-			create(getInsertCellAddress(&head, itr), val);
+			create(CellAddress(&initial, itr), val);
 			printf("\n要素\"%s\"が%d番目に挿入されました。\n", val, itr);
 		}
-		selectedControl = 0;
+		select = 0;
 		break;
 
 		case 3:
-			if (GetSize(&head) > 0)
+			if (GetSize(&initial) > 0)
 			{
 				int itr;
 				char val[64];
-
 				printf("\n[要素の編集]\n編集したい要素の番号を指定してください。\n");
 				scanf_s("%d", &itr);
 				itr = max(0, itr);
-				if (GetSize(&head) > itr)
+				if (GetSize(&initial) > itr)
 				{
 					printf("\n%d番目の要素の変更する値を入力してください。\n", itr);
 					scanf_s("%s", val, 64);
 
-					replace(getInsertCellAddress(&head, itr + 1), val);
+					move(CellAddress(&initial, itr + 1), val);
 					printf("\n%d番目の要素の値が\"%s\"に変更されました。\n", itr, val);
 				}
 				else
@@ -141,20 +138,19 @@ int main()
 					printf("\n%d番目の要素が見つかりませんでした。\n", itr);
 				}
 			}
-			selectedControl = 0;
+			select = 0;
 			break;
 
 		case 4:
-			if (GetSize(&head) > 0)
+			if (GetSize(&initial) > 0)
 			{
 				int itr;
-
 				printf("\n[要素の削除]\n削除したい要素の番号を指定してください。\n");
 				scanf_s("%d", &itr);
 				itr = max(0, itr);
-				if (GetSize(&head) > itr)
+				if (GetSize(&initial) > itr)
 				{
-					erase(getInsertCellAddress(&head, itr + 1));
+					out(CellAddress(&initial, itr + 1));
 					printf("\n%d番目の要素を削除しました。\n", itr);
 				}
 				else
@@ -162,11 +158,11 @@ int main()
 					printf("\n%d番目の要素が見つかりませんでした。\n", itr);
 				}
 			}
-			selectedControl = 0;
+			select = 0;
 			break;
 
 		case 5:
-			if (GetSize(&head) > 0)
+			if (GetSize(&initial) > 0)
 			{
 				int itr;
 				int itr2;
@@ -174,14 +170,14 @@ int main()
 				printf("\n[要素の並び替え]\n入れ替える一方の要素の番号を指定してください。\n");
 				scanf_s("%d", &itr);
 				itr = max(0, itr);
-				if (GetSize(&head) > itr)
+				if (GetSize(&initial) > itr)
 				{
 					printf("\n%d番目の要素と入れ替える要素の番号を指定してください。\n", itr);
 					scanf_s("%d", &itr2);
 					itr2 = max(0, itr2);
-					if (GetSize(&head) > itr2)
+					if (GetSize(&initial) > itr2)
 					{
-						swap(getInsertCellAddress(&head, itr + 1), getInsertCellAddress(&head, itr2 + 1));
+						change(CellAddress(&initial, itr + 1), CellAddress(&initial, itr2 + 1));
 						printf("\n%d番目の要素と%d番目の要素を入れ替えました。\n", itr, itr2);
 					}
 					else
@@ -194,15 +190,14 @@ int main()
 					printf("\n%d番目の要素が見つかりませんでした。\n", itr);
 				}
 			}
-			selectedControl = 0;
+			select = 0;
 			break;
 		}
 	}
-
 	return 0;
 }
 
-CELL* getInsertCellAddress(CELL* endCELL, int iterator)
+CELL* CellAddress(CELL* endCELL, int iterator)
 {
 	for (int i = 0; i < iterator; i++)
 	{
@@ -218,49 +213,38 @@ CELL* getInsertCellAddress(CELL* endCELL, int iterator)
 	return endCELL;
 }
 
-void create(CELL* currentCell, char val[64])
+void create(CELL* JustCell, char val[64])
 {
 	CELL* newCell;
 	newCell = (CELL*)malloc(sizeof(CELL));
-
+	newCell->next = JustCell->next;
+	newCell->preview = JustCell;
 	strcpy_s(newCell->val, 64, val);
-	newCell->prev = currentCell;
-	newCell->next = currentCell->next;
 
-	if (currentCell->next)
+	if (JustCell->next)
 	{
-		CELL* nextCell = currentCell->next;
-		nextCell->prev = newCell;
+		CELL* NextCell = JustCell->next;
+		NextCell->preview = newCell;
 	}
-
-	currentCell->next = newCell;
+	JustCell->next = newCell;
 }
 
-void erase(CELL* currentCell)
+void out(CELL* JustCell)
 {
-	CELL* prevCell = currentCell->prev;
-	prevCell->next = currentCell->next;
+	CELL* PreviewCell = JustCell->preview;
+	PreviewCell->next = JustCell->next;
 
-	if (currentCell->next)
+	if (JustCell->next)
 	{
-		CELL* nextCell = currentCell->next;
-		nextCell->prev = currentCell->prev;
+		CELL* NextCell = JustCell->next;
+		NextCell->preview = JustCell->preview;
 	}
-
-	delete currentCell;
+	delete JustCell;
 }
 
-void replace(CELL* currentCell, char val[64])
+void move(CELL* JustCell, char val[64])
 {
-	strcpy_s(currentCell->val, 64, val);
-}
-
-void swap(CELL* CellA, CELL* CellB)
-{
-	char swapVal[64];
-	strcpy_s(swapVal, 64, CellA->val);
-	strcpy_s(CellA->val, 64, CellB->val);
-	strcpy_s(CellB->val, 64, swapVal);
+	strcpy_s(JustCell->val, 64, val);
 }
 
 void index(CELL* endCell)
@@ -282,11 +266,18 @@ int GetSize(CELL* endCell)
 		endCell = endCell->next;
 		no++;
 	}
-
 	return no;
 }
 
-void StatingMenu()
+void change(CELL* cellA, CELL* cellB)
 {
-	printf("-------------------------------\n1. 要素の表示に戻る\n2. 要素の操作に戻る\n");
+	char ChangeVal[64];
+	strcpy_s(ChangeVal, 64, cellA->val);
+	strcpy_s(cellA->val, 64, cellB->val);
+	strcpy_s(cellB->val, 64, ChangeVal);
+}
+
+void Begining()
+{
+	printf("------------------------\n1. 要素の表示に戻る\n2. 要素の操作に戻る\n");
 }
