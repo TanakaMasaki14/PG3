63edf46a6fea154418ac0d45ebe2035e651d0e06
tanaka_masaki <k021g1449@g.neec.ac.jp>
2023-01-04 02:35:50 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,17 +1,49 @@
+#include <stdio.h>
+#include <list>
+#include <memory>
 
-#include"Circle.h"
-#include"Rectangle.h"
+#define PI 3.14159265358;
+
+class IShape
+{
+public:
+	IShape() {};
+	~IShape() {};
+	virtual float size() = 0;
+	virtual void draw() = 0;
+};
+
+class Circle : public IShape
+{
+public:
+	Circle(float r) { Circle::r = r; }
+	~Circle() {};
+	float size() override { return r * r * PI; };
+	void draw() override { printf("â~ÇÃñ êœ:%f\n", size()); }
+private:
+
+	float r;
+};
+
+class Rectangle : public IShape
+{
+public:
+	Rectangle(float x, float y) { Rectangle::x = x; Rectangle::y = y; }
+	~Rectangle() {};
+	float size() override { return x * y; };
+	void draw() override { printf("ãÈå`ÇÃñ êœ:%f\n", size()); }
+private:
+
+	float x, y;
+};
 
 int main()
 {
-	IShape* iShape[2];
-	iShape[0] = new Circle(20.0f);
-	iShape[1] = new Rectangle(10.0f, 5.0f);
+	std::unique_ptr<Circle>circle = std::make_unique<Circle>(4);
+	std::unique_ptr<Rectangle>rectangle = std::make_unique<Rectangle>(4, 5);
+	circle->draw();
+	rectangle->draw();
 
-	for (IShape* iShapes : iShape)
-	{
-		iShapes->Draw();
-	}
-
+	system("pause");
 	return 0;
 }
